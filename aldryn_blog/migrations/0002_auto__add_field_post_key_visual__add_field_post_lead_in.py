6b3df622c38a94387e21aeac234b15ede3bc103e
Adam Bogdal <adam@bogdal.pl>
2013-08-14 16:32:28 +0200

    Run firstly dependent migration

--- aldryn_blog/migrations/0002_auto__add_field_post_key_visual__add_field_post_lead_in.py
+++ aldryn_blog/migrations/0002_auto__add_field_post_key_visual__add_field_post_lead_in.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("filer", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Post.key_visual'
         db.add_column(u'aldryn_blog_post', 'key_visual',
