74fba7344a5b21e0877d1473f9980076d1b68133
FinalAngel <angelo.dini@divio.ch>
2013-10-23 20:10:08 +0200

    remove navigation templatetag

--- aldryn_blog/templatetags/aldryn_blog_tags.py
+++ aldryn_blog/templatetags/aldryn_blog_tags.py
@@ -6,26 +6,6 @@ from django import template
 
 register = template.Library()
 
-from classytags.helpers import InclusionTag
-
-from aldryn_blog.models import Post
-
-
-class ArchiveNavigation(InclusionTag):
-    template = 'aldryn_blog/includes/archive_navigation.html'
-
-    options = Options(
-        Flag('full_navigation', true_values=['full_navigation'], default=False),
-    )
-
-    def get_context(self, context, **kwargs):
-        context['dates'] = Post.published.dates('publication_start', 'month')
-        context.update(kwargs)
-        return context
-
-register.tag('archive_navigation', ArchiveNavigation)
-
-
 @register.filter
 def posts(latest, objects):
     if latest:
