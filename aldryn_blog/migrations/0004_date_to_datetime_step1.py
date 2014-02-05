affb0d267f5dd7d466598d4e3c645ba32ac7fb6c
Patrick Lauber <digi@treepy.com>
2014-02-05 11:31:49 +0100

    fixed migrations

--- aldryn_blog/migrations/0004_date_to_datetime_step1.py
+++ aldryn_blog/migrations/0004_date_to_datetime_step1.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from django.utils import timezone
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'Post.publication_start'
         db.add_column(u'aldryn_blog_post', 'publication_start',
-                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now),
+                      self.gf('django.db.models.fields.DateTimeField')(default=timezone.now),
                       keep_default=False)
 
         # Adding field 'Post.publication_end'
