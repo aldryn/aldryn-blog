3612532fb4744b3120120e690a7b427d103725cf
Paulo <commonzenpython@gmail.com>
2014-05-29 12:31:10 -0400

    adds helper template tag

--- aldryn_blog/templatetags/aldryn_blog_tags.py
+++ aldryn_blog/templatetags/aldryn_blog_tags.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from classytags.arguments import Flag
-from classytags.core import Options
-
 from django import template
+from django.core.urlresolvers import reverse
 
 register = template.Library()
+
 
 @register.filter
 def posts(latest, objects):
@@ -18,3 +17,15 @@ def user_name(user):
     if user.get_full_name():
         return user.get_full_name()
     return user.username
+
+
+@register.assignment_tag
+def get_blog_post_tags(post):
+    """
+    Returns a list of tags for post, with an extra url attribute.
+    """
+    post_tags = list(post.tags.all())
+
+    for tag in post_tags:
+        tag.get_absolute_url = reverse('aldryn_blog:tagged-posts', kwargs={'tag': tag.slug})
+    return post_tags
