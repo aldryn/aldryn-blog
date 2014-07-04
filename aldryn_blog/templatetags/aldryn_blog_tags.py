60ba2473e8a458153f2f48d85ebe6153672571cb
Kim Thoenen <kim@smuzey.ch>
2014-07-04 09:57:26 +0200

    template tag to fetch related posts

--- aldryn_blog/templatetags/aldryn_blog_tags.py
+++ aldryn_blog/templatetags/aldryn_blog_tags.py
@@ -2,6 +2,9 @@
 from django import template
 from django.core.urlresolvers import reverse
 
+from ..models import Post
+
+
 register = template.Library()
 
 
@@ -29,3 +32,66 @@ def get_blog_post_tags(post):
     for tag in post_tags:
         tag.get_absolute_url = reverse('aldryn_blog:tagged-posts', kwargs={'tag': tag.slug})
     return post_tags
+
+
+@register.assignment_tag
+def get_related_posts(post, by_categories=True, by_tags=True, by_latest=True, wanted_count=5):
+    """
+    Returns a list of blog objects being related to the one given.
+
+    "Related" can mean one or multiple of the following conditions
+    (being configurable by the arguments passed to this function):
+     - In the same category
+     - Having one or more tags in common
+     - Just being released recently
+
+    Another rule to follow:
+     - found_objects == wanted_count => return found_objects
+     - found_objects < wanted_count => fill the missing count with the next condition
+     - found_objects > wanted_count => further trim down found_objects by the next condition
+    """
+
+    given_category = post.category_id
+    given_tags = post.tags.values_list('pk', flat=True)
+    given_language = post.language
+
+    found = None
+    all_posts = Post.objects.filter(language=given_language).order_by('publication_start')
+
+    if by_categories:
+        posts_by_category = all_posts.filter(category_id=given_category)
+
+        if posts_by_category.count() == wanted_count:
+            return list(posts_by_category)
+
+        found = posts_by_category
+
+    if by_tags:
+        append = False
+        if found:
+            if found.count() > wanted_count:
+                qs = found
+            else:
+                append = True
+                qs = all_posts
+            found = list(found)
+        else:
+            qs = all_posts
+
+        posts_by_tags = list(qs.filter(tags__in=given_tags))
+
+        if posts_by_tags:
+            if append:
+                found += posts_by_tags
+            else:
+                found = posts_by_tags
+
+            if len(found) >= wanted_count:
+                return posts_by_tags[:wanted_count]
+
+    if by_latest:
+        if not found:
+            found = []
+        return found + list(all_posts[:wanted_count - len(found)])
+
+    return found
